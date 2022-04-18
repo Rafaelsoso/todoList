5dbc9e68525e709eb95d017165ca56566bc4d474
Anh Ha Kieu <anh.ha@sotatek.com>
2022-04-18 16:06:27 +0700

    add initial project

---
+++ SwiftUI_TodoList/SwiftUI_TodoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_TodoList
+//
+//  Created by Cosette Nguyen on 18/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_TodoList/SwiftUI_TodoList/SwiftUI_TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TodoListApp.swift
+//  SwiftUI_TodoList
+//
+//  Created by Cosette Nguyen on 18/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
